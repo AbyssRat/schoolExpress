23b0579fab3b2264043e4d958b68d95698737e8b
AbyssRat <yunothefurry@gmail.com>
2025-11-27 11:17:30 +0100

    courses type shi

--- api/courses.js
+++ api/courses.js
@@ -3,3 +3,97 @@ import * as coursesModel from '../model/coursesModel.js';
 const coursesRouter = express.Router();
 
 //CRUD (Create, Read, Update, Delete) műveleteket REST végpontokon keresztül.
+//id, title, description
+coursesRouter.post('/', async (req, res) => {
+    try {
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).send({ error: 'Hiányzó adatok.' });
+        }
+
+        const { title, description } = req.body;
+
+        if (!title || !description) {
+            return res.status(400).send({
+                error: 'Cím és Leírás megadása kötelező.'
+            });
+        }
+
+        const result = await coursesModel.insertCourse(req.body);
+        res.status(201).send({
+            message: 'Kurzus sikeresen hozzáadva.',
+            id: result.insertId
+        });
+    } catch (error) {
+        console.error('Hiba a kurzus hozzáadásakor:', error);
+        res.status(501).send({
+            error: 'Hiba történt a kurzus hozzáadása során.'
+        });
+    }
+});
+
+//get
+
+coursesRouter.get('/', async (req, res) => {
+    try {
+        const courses = await coursesModel.getAllCourses();
+        res.status(201).send(courses);
+    } catch (error) {
+        console.error('Hiba a kurzusok lekérésekor:', error);
+        res.status(501).send({ error: 'Hiba történt a kurzusok lekérése során.' });
+    }
+});
+
+//put
+
+coursesRouter.put('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).send({ error: 'Hiányzó adatok.' });
+        }
+
+        const { title, description } = req.body;
+
+        if (!title || !description) {
+            return res.status(400).send({
+                error: 'Cím és Leírás megadása kötelező.'
+            });
+        }
+
+        const result = await coursesModel.updateCourse(id, req.body);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).send({ error: 'Kurzus nem található.' });
+        }
+
+        res.status(200).send({ message: 'Kurzus sikeresen frissítve.' });
+    } catch (error) {
+        console.error('Hiba a kurzus frissítésekor:', error);
+        res.status(501).send({
+            error: 'Hiba történt a kurzus frissítése során.'
+        });
+    }
+});
+
+//delete
+
+coursesRouter.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await coursesModel.deleteCourse(id);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).send({ error: 'Kurzus nem található.' });
+        }
+
+        res.status(200).send({ message: 'Kurzus sikeresen törölve.' });
+    } catch (error) {
+        console.error('Hiba a kurzus törlésekor:', error);
+        res.status(501).send({
+            error: 'Hiba történt a kurzus törlése során.'
+        });
+    }
+});
+
+export default coursesRouter;
--- model/coursesModel.js
+++ model/coursesModel.js
@@ -1,3 +1,34 @@
 import pool from '../db.js';
 
 //CRUD (Create, Read, Update, Delete) műveleteket REST végpontokon keresztül.
+
+export const getAllCourses = async () => {
+    const [rows] = await pool.query('SELECT * FROM courses');
+    return rows;
+}
+
+export const insertCourse = async (course) => {
+    const { title, description } = course;
+    const [result] = await pool.query(
+        'INSERT INTO courses (title, description) VALUES (?, ?)',
+        [title, description]
+    );
+    return result;
+}
+
+export const updateCourse = async (id, course) => {
+    const { title, description } = course;
+    const [result] = await pool.query(
+        'UPDATE courses SET title = ?, description = ? WHERE id = ?',
+        [title, description, id]
+    );
+    return result;
+};
+
+export const deleteCourse = async (id) => {
+    const [result] = await pool.query(
+        'DELETE FROM courses WHERE id = ?',
+        [id]
+    );
+    return result;
+};
